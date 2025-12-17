9cddc35d8434a6af4f1b4c6396dfdaa6f5df9c8f
jrdeavila <morenojr15@hotmail.com>
2025-12-17 08:04:15 -0500

    Add typing to export tickets controller

--- app/Http/Controllers/TicketExportController.php
+++ app/Http/Controllers/TicketExportController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Exports\TicketsExport;
 use App\Http\Requests\TicketExportRequest;
 use Maatwebsite\Excel\Facades\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class TicketExportController extends Controller
 {
-    public function __invoke(TicketExportRequest $request)
+    public function __invoke(TicketExportRequest $request): BinaryFileResponse
     {
         $start = $request->input('start_date');
         $end   = $request->input('end_date');
