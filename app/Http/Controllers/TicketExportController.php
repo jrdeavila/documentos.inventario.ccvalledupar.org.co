105367606feea02a5f57f777256fbd1e6779ba4c
jrdeavila <morenojr15@hotmail.com>
2026-01-16 19:42:58 +0000

    Refactor TicketExportController to use BinaryFileResponse and streamline export logic

--- app/Http/Controllers/TicketExportController.php
+++ app/Http/Controllers/TicketExportController.php
@@ -6,35 +6,24 @@ use App\Exports\TicketsExport;
 use App\Http\Requests\TicketExportRequest;
 use Illuminate\Http\JsonResponse;
 use Maatwebsite\Excel\Facades\Excel;
-use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class TicketExportController extends Controller
 {
-    public function __invoke(TicketExportRequest $request): StreamedResponse | JsonResponse
+    public function __invoke(TicketExportRequest $request): JsonResponse | BinaryFileResponse
     {
         $start = $request->input('start_date');
         $end   = $request->input('end_date');
 
-        $fileName = (!$start && !$end) ? "tickets.xlsx" : "tickets_{$start}_a_{$end}.xlsx";
-
         $export = new TicketsExport($start, $end);
+
         if ($export->query()->count() === 0) {
             return response()->json(['message' => 'No hay tickets para exportar'], 404);
         }
 
-        $export = Excel::download($export, $fileName);
+        $fileName = (!$start && !$end) ? "tickets.xlsx" : "tickets_{$start}_a_{$end}.xlsx";
 
-        $file = $export->getFile();
-
-        $filename = "tickets_{$start}_a_{$end}.xlsx";
-
-        return response()->streamDownload(
-            fn() => fopen($file->getPathname(), 'r+'),
-            $filename,
-            [
-                'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-
-            ]
-        );
+        // Excel::download ya devuelve la respuesta correcta con los headers necesarios
+        return Excel::download($export, $fileName);
     }
 }
